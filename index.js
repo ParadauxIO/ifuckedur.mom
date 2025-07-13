3b0109a3692bef5f04e45cacc13d81f30701a5d1
Rían Errity <rian@paradaux.io>
2025-07-13 22:07:08 +0100

    Update site to match corresponding API changes

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import {fileURLToPath} from "url";
 import ejs from "ejs";
 import fs from "fs";
 import {logMiddleware} from "./middleware/log.js";
+import {createVisit} from "./middleware/visit.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -26,34 +27,8 @@ if (!fs.existsSync(logsDir)) {
 app.use(logMiddleware(logsDir));
 
 // Increment the visitor count
-app.use((req, res, next) => {
-    const ignoredUserAgents = ["bot", "crawler", "spider", "scanner", "curl", "wget", "python", "java", "php", 'unknown', 'discord', '(compatible)', 'zgrab'];
-    // if the user agent contains any of the ignored user agents, skip incrementing the counter
-    const userAgent = req.headers['user-agent']?.toLowerCase() || 'Unknown';
-    if (ignoredUserAgents.some(ua => userAgent.toLowerCase().includes(ua))) {
-        return next();
-    }
+app.use(createVisit(process.env.PARADAUX_API_BASE_URL, process.env.PARADAUX_API_SECRET));
 
-    // Increment the human visit counter
-    if (req.method === 'GET' && req.path === '/') {
-        let ip = req.headers['x-forwarded-for'] ||
-            req.headers['x-real-ip'] ||
-            req.socket.remoteAddress;
-
-        fetch(process.env.PARADAUX_API_BASE_URL + "/api/ifum/visit", {
-            method: "POST",
-            headers: {
-                'X-SECRET': process.env.PARADAUX_API_SECRET,
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                ipAddress: ip,
-                userAgent: req.headers['user-agent'] || 'Unknown',
-            })
-        }).catch(err => console.error(err));
-    }
-    next();
-});
 app.get("/", (req, res) => {
     res.render("Index", {
         apiBaseUrl: process.env.PARADAUX_API_BASE_URL
